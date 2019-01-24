ee04e6622ff7734847fdd7b6b67ee4413374ee61
cmcorrales <cmcorrales3@gmail.com>
2019-01-24 17:30:43 -0500

    add event listener to calendar date cells

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,26 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// function overlay() {
+// 	el = document.getElementById("overlay");
+// 	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+// }
+//
+// <a href='#' onclick='overlay()'>Click here to show the overlay</a>
+//
+// document.getElementById("myBtn").addEventListener("click", displayDate)
+
+// for(let i=1; i < 32; i++) {
+//     document.getElementById(`${i}`).addEventListener("click", console.log("clicked", i))
+// }
+
+showModal(date) {
+  console.log(date)
+}
+
+document.addEventListener('click', function(e) {
+  if(e.target && !!isNaN(event.target.id)) {
+    showModal(event.target.id)
+  }
+})
