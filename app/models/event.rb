6026c0e26085f811b7c719ec42bfde698ad84dbe
cmcorrales <cmcorrales3@gmail.com>
2019-01-24 23:37:08 -0500

    added validation to ensure uniqueness of event start time on any given day

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,23 +1,14 @@
 class Event < ApplicationRecord
   validates :start_time, :end_time, :date, presence: true
-  validate :start_time_cannot_be_greater_than_end_time, :event_start_time_cannot_repeat
+  validate :start_time_cannot_be_greater_than_end_time
+  validates :start_time, uniqueness: { scope: :date,
+  message: "Start time error: Event already exists at selected start time." }
+
 
   def start_time_cannot_be_greater_than_end_time
     if start_time > end_time
       errors.add(:start_time, "must be greater than or equal to end time")
     end
   end
-
-
-  # def event_start_time_cannot_repeat
-  #   events_with_same_date = Event.all.select do |event|
-  #     event.date == date
-  #   end
-  #   events_with_same_date.each do |event|
-  #     if event.start_time == start_time
-  #         errors.add(:start_time, "must be unique for a given day")
-  #     end
-  #   end
-  # end
 
 end
